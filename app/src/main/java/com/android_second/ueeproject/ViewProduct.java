7029bd19b4fac8dd0bb74730833e51042ff54989
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-10-07 15:31:47 +0530

    add tab layout

--- app/src/main/java/com/android_second/ueeproject/ViewProduct.java
+++ app/src/main/java/com/android_second/ueeproject/ViewProduct.java
@@ -1,17 +1,74 @@
 package com.android_second.ueeproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.Objects;
 
 public class ViewProduct extends AppCompatActivity {
-
+    private ViewPager2 viewPager2;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+//        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        viewPager2=findViewById(R.id.view_pager);
+        viewPager2.setAdapter(new ProductPageAdapter(this));
+        tabLayout=findViewById(R.id.tabLayout);
+        TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0: {
+                        tab.setText("OverView");
+                        tab.setIcon(R.drawable.user_overview_black);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+                        badgeDrawable.setVisible(true);
+
+                        break;
+                    }
+                    case 2: {
+                        tab.setText("Rating");
+                        tab.setIcon(R.drawable.user_rating_black);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+                        badgeDrawable.setVisible(true);
+                        badgeDrawable.setNumber(8);
+                        badgeDrawable.setMaxCharacterCount(3);
+                        break;
+                    }
+                    case 1: {
+                        tab.setText("Product Details");
+                        tab.setIcon(R.drawable.user_product_overview_black);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+                        badgeDrawable.setVisible(true);
+                        badgeDrawable.setNumber(100);
+                        badgeDrawable.setMaxCharacterCount(3);
+                        break;
+                    }
+                }
+            }
+        });
+        tabLayoutMediator.attach();
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                BadgeDrawable badgeDrawable=tabLayout.getTabAt(position).getOrCreateBadge();
+                badgeDrawable.setVisible(false);
+
+            }
+        });
     }
 }
