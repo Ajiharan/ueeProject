189e8722e055b426d54da927156a7fb00a5bd2e2
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-10 18:37:10 +0530

    Add Card details

---
+++ app/src/main/java/com/android_second/ueeproject/AddCardDetail.java
@@ -0,0 +1,14 @@
+package com.android_second.ueeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCardDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card_detail);
+    }
+}
