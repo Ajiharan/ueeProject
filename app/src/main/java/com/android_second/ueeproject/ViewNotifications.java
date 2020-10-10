7a0367d436ecdefc2b294fbfa57de5b3844dc181
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-10 17:45:49 +0530

    Notifications

---
+++ app/src/main/java/com/android_second/ueeproject/ViewNotifications.java
@@ -0,0 +1,14 @@
+package com.android_second.ueeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNotifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_notifications);
+    }
+}
