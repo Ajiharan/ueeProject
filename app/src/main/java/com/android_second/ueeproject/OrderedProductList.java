0cbbed7107a0c6664fdde05f0b70fe964f634693
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-10 17:47:23 +0530

    Order Confirmation

---
+++ app/src/main/java/com/android_second/ueeproject/OrderedProductList.java
@@ -0,0 +1,14 @@
+package com.android_second.ueeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderedProductList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordered_product_list);
+    }
+}
