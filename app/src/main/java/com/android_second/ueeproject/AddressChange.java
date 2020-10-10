8325b9caa0135d9f67657aa633fb407f7798b317
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-10 18:58:03 +0530

    AddressChange UI

---
+++ app/src/main/java/com/android_second/ueeproject/AddressChange.java
@@ -0,0 +1,14 @@
+package com.android_second.ueeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddressChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_change);
+    }
+}
