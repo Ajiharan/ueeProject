a4543520c2265bbb25ec7a89b4afe7aa1319c322
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-10-10 19:45:31 +0530

    fixed issues

--- app/src/main/java/com/android_second/ueeproject/CartActivity.java
+++ app/src/main/java/com/android_second/ueeproject/CartActivity.java
@@ -2,13 +2,18 @@ package com.android_second.ueeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class CartActivity extends AppCompatActivity {
     private Spinner spin1,spin2,spin3;
-
+    private ImageView img_arrow;
+    private Button btnProceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,24 @@ public class CartActivity extends AppCompatActivity {
         spin1=findViewById(R.id.spin1);
         spin2=findViewById(R.id.spin2);
         spin3=findViewById(R.id.spin3);
-
+        img_arrow=findViewById(R.id.img_arrow);
+        btnProceed=findViewById(R.id.btnProceed);
+        btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), Payment.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+        img_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), HomeScreen.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
         addSpinner(spin1);
         addSpinner(spin2);
         addSpinner(spin3);
