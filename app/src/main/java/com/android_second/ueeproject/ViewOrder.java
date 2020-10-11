6e27798269a967d63a6e5fcbcbd2c41ca5c3eee4
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-11 20:24:57 +0530

    Orders UI

--- app/src/main/java/com/android_second/ueeproject/ViewOrder.java
+++ app/src/main/java/com/android_second/ueeproject/ViewOrder.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OrderedProductList extends AppCompatActivity {
+public class ViewOrder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ordered_product_list);
+        setContentView(R.layout.activity_view_order);
     }
 }
--- app/src/main/java/com/android_second/ueeproject/ViewOrders.java
+++ app/src/main/java/com/android_second/ueeproject/ViewOrders.java
@@ -2,13 +2,27 @@ package com.android_second.ueeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ViewOrders extends AppCompatActivity {
 
+    LinearLayout layout1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_orders);
+
+        layout1 = findViewById(R.id.layout1);
+
+        layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ViewOrders.this,ViewOrder.class);
+                startActivity(intent);
+            }
+        });
     }
 }
