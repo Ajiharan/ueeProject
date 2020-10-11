8106b7c52f893e4a888f95586059b9bdb40ae78c
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-11 12:11:25 +0530

    Add card UI

--- app/src/main/java/com/android_second/ueeproject/Payment.java
+++ app/src/main/java/com/android_second/ueeproject/Payment.java
@@ -7,21 +7,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class Payment extends AppCompatActivity {
 
     private Button btnChange;
+    private RadioButton btnCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
         btnChange = findViewById(R.id.button_change_address);
-
         btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Payment.this, AddressChange.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCard  = findViewById(R.id.radio_btn_card);
+        btnCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Payment.this, AddCardDetail.class);
                 startActivity(intent);
             }
         });
