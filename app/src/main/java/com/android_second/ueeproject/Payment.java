ce1d3a3806d66033383f0b891b18136f959198bb
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-11 11:49:15 +0530

    Payment UI

--- app/src/main/java/com/android_second/ueeproject/Payment.java
+++ app/src/main/java/com/android_second/ueeproject/Payment.java
@@ -2,13 +2,28 @@ package com.android_second.ueeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Payment extends AppCompatActivity {
 
+    private Button btnChange;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        btnChange = findViewById(R.id.button_change_address);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Payment.this, AddressChange.class);
+                startActivity(intent);
+            }
+        });
     }
 }
