070984de87eecc655c3a2612bf7919beeb156db5
Nirogini23 <53430717+Nirogini23@users.noreply.github.com>
2020-10-11 21:09:23 +0530

    Order Confirmation UI

--- app/src/main/java/com/android_second/ueeproject/Payment.java
+++ app/src/main/java/com/android_second/ueeproject/Payment.java
@@ -2,21 +2,37 @@ package com.android_second.ueeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.app.Notification;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Payment extends AppCompatActivity {
 
     private Button btnChange;
     private RadioButton btnCard;
+    private Button order_place;
+
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        dialog = new Dialog(this);
+
+        order_place = findViewById(R.id.button_place_order);
+        order_place.setOnClickListener((view) ->{
+            OrderConfirmation();
+        });
 
         btnChange = findViewById(R.id.button_change_address);
         btnChange.setOnClickListener(new View.OnClickListener() {
@@ -35,5 +51,29 @@ public class Payment extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void OrderConfirmation() {
+        TextView txtclose;
+        Button OK;
+        dialog.setContentView(R.layout.order_confirmation_popup);
+        txtclose = (TextView) dialog.findViewById(R.id.close);
+        OK = (Button) dialog.findViewById(R.id.ok);
+
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
