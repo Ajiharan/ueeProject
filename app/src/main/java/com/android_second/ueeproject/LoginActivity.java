994f9f3c04afe9f55f42c0120e5c7f75126de5f2
Varniah88 <53952052+Varniah88@users.noreply.github.com>
2020-10-22 23:46:02 +0530

    Changes

--- app/src/main/java/com/android_second/ueeproject/LoginActivity.java
+++ app/src/main/java/com/android_second/ueeproject/LoginActivity.java
@@ -8,9 +8,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -28,11 +34,44 @@ public class LoginActivity extends AppCompatActivity {
         Button skip_login = findViewById(R.id.skiplogin);
         TextView create_account = findViewById(R.id.create_account);
         TextView forget_password = findViewById(R.id.forgetpassword);
+        EditText username = findViewById(R.id.enteruser);
+        EditText password = findViewById(R.id.enter_password);
+        CheckBox show_pw = findViewById(R.id.checkBox);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openHomeActivity();
+
+                final String Name = username.getText().toString();
+                final  String Pass = password.getText().toString();
+                if(Name.length()==0)
+                {
+                    username.requestFocus();
+                    username.setError("USER NAME CANNOT BE EMPTY");
+                }
+                else if(Pass.length()==0)
+                {
+                    password.requestFocus();
+                    password.setError("PASSWORD CANNOT BE EMPTY");
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Successfully Logged In!",Toast.LENGTH_LONG).show();
+                    openHomeActivity();
+                }
+
+
+            }
+        });
+
+        show_pw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else{
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
             }
         });
 
--- app/src/main/java/com/android_second/ueeproject/RegisterActivity.java
+++ app/src/main/java/com/android_second/ueeproject/RegisterActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +24,44 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button register = findViewById(R.id.register_user);
         TextView login = findViewById(R.id.log);
+        EditText username = findViewById(R.id.enterusername);
+        EditText email = findViewById(R.id.editTextTextPersonName2);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        EditText confirmpw = findViewById(R.id.editTextTextPersonName3);
+        CheckBox show_pw = findViewById(R.id.checkBox2);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openHomeActivity();
+
+                final String Name = username.getText().toString();
+                final  String Pass = password.getText().toString();
+                final String Email = email.getText().toString();
+                final String ConPw = confirmpw.getText().toString();
+                if(Name.length()==0)
+                {
+                    username.requestFocus();
+                    username.setError("USER NAME CANNOT BE EMPTY");
+                }
+                else if (!Email.matches(emailPattern)){
+
+                    email.requestFocus();
+                    email.setError("INVALID E-MAIL ADDRESS");
+                }
+                else if(Pass.length()==0)
+                {
+                    password.requestFocus();
+                    password.setError("PASSWORD CANNOT BE EMPTY");
+                }
+                else if(!ConPw.matches(Pass)){
+                    confirmpw.requestFocus();
+                    confirmpw.setError("PASSWORD IS UNMATCHED");
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this,"Successfully Created Account!",Toast.LENGTH_LONG).show();
+                    openHomeActivity();
+                }
+
             }
         });
 
@@ -29,6 +69,20 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openLoginActivity();
+            }
+        });
+
+        show_pw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    confirmpw.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else{
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    confirmpw.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
             }
         });
 
